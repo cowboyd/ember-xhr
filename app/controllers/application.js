29272eb693438c2136f1da43d36a1c04b8e8a9f4
Charles Lowell <cowboyd@frontside.io>
2014-09-12 15:19:02 -0500

    make the input binding a little more clear

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,18 +5,24 @@ export default Ember.Controller.extend({
   xhr: XHR.create(),
   bindUploadFile: Ember.observer(function() {
     this.set('uploadFile', function(e) {
+      this.send('upload', e.target.files[0]);
+    }.bind(this));
+  }).on('init'),
+
+  actions: {
+    abort: function() {
+      this.get('xhr').abort();
+    },
+    upload: function(file) {
       var xhr = this.get('xhr');
 
-      var file = e.target.files[0];
       var data = new FormData();
       data.append('file', file, file.name);
       xhr.open('POST', 'http://posttestserver.com/post.php');
       xhr.send(data);
-    }.bind(this));
-  }).on('init'),
-  actions: {
-    abort: function() {
-      this.get('xhr').abort();
+      xhr.then(function() {
+        console.log('done!')
+      })
     }
   }
 });
